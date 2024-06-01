94bbddc358555a41278517408844796db3e0ac03
jfpn1920cr <162055100+jfpn1920cg@users.noreply.github.com>
2024-06-01 07:50:59 -0500

    contenido_finalizado

--- actividad/ejercicio_rsa.py
+++ actividad/ejercicio_rsa.py
@@ -1,24 +1,23 @@
 import random
 import sympy
 def generar_claves(tamaño_bits):
-    # Generar números primos grandes p y q
+    #generar_numeros_primos_grandes_p_y_q
     p = sympy.randprime(2**(tamaño_bits//2 - 1), 2**(tamaño_bits//2))
     q = sympy.randprime(2**(tamaño_bits//2 - 1), 2**(tamaño_bits//2))
-    # Calcular n y φ(n)
+    #calcular_n_y_φ(n)
     n = p * q
     phi_n = (p - 1) * (q - 1)
-    # Elegir un exponente de cifrado e
+    #elegir_un_exponente_de_cifrado_e
     e = random.randint(2, phi_n - 1)
     while sympy.gcd(e, phi_n) != 1:
         e = random.randint(2, phi_n - 1)
-    # Calcular el exponente de descifrado d
+    #calcular_el_exponente_de_descifrado_d
     d = sympy.mod_inverse(e, phi_n)
-    # Clave pública: (e, n), Clave privada: (d, n)
+    #clave_publica_(e_n)_Clave_privada_(d_n)
     clave_publica = (e, n)
     clave_privada = (d, n)
     return clave_publica, clave_privada
-# Ejemplo de uso
-tamaño_bits = 124  # Tamaño de las claves en bits
+tamaño_bits = 124 #tamaño_de_las_claves_en_bits
 clave_publica, clave_privada = generar_claves(tamaño_bits)
 print("Clave pública:", clave_publica)
 print("Clave privada:", clave_privada)
